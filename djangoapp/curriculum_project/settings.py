78bf06956ed8120b62e0d5189bbef883a31043aa
Arthur Souza <arthurviniciussouza906@gmail.com>
2024-02-03 11:54:13 -0400

    update tempaltes

--- djangoapp/curriculum_project/settings.py
+++ djangoapp/curriculum_project/settings.py
@@ -134,7 +134,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = DATA_DIR / 'static' 
+STATIC_ROOT = os.path.join(DATA_DIR, "staticfiles")
+
+STATICFILES_DIRS = (
+    os.path.join(DATA_DIR, "static"),
+)
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = DATA_DIR / 'media'
 
